829036daf800f23e1e5ef15d9753f2831956fd4c
HARSHAKUMAR REDDY BALIREDDYGARI <102666296+Harshahari1207@users.noreply.github.com>
2023-01-31 13:57:39 +0530

    Clean code and usefull Comments

--- src/inboxElements.js
+++ src/inboxElements.js
@@ -1,16 +1,14 @@
-import displayToPage from "./display";
+// import displayToPage from "./display";
 
 const taskForm = document.querySelector('.taskForm');
 const inboxBtn = document.querySelector('.btnInbox');
+
+// showing Form to fill the todo list 
 export default function inboxElementsView(){
     inboxBtn.addEventListener('click', formDisplay);
     function formDisplay(){
         taskForm.style.display = 'flex';
     }
-   
-    // displayToPage();
-    
-
 }
 
 
--- src/inboxFun.js
+++ src/inboxFun.js
@@ -4,37 +4,35 @@ const taskForm = document.querySelector('.taskForm');
 const form = document.querySelector('.form');
 
 
-
+// Empty Array
 export let todoList = [
 ];
 
+// Cunstructor
 function List(title, date, note){
     this.title = title;
     this.date = date;
     this.note = note;
 }
 
-// function addListToTodolist(title, date, note){
-//     let list = new List(title, date, note);
-//     todoList.unshift(list)
-// }
-
-// addListToTodolist("Hari", '31/1/2023', "alishdoiaoihdo nona");
+// Closing Form
 function closeForm(){
     taskForm.style.display = 'none';
     event.preventDefault();
 }
+// Resetting the form
 function resetForm(){
     form.reset();
     event.preventDefault();
 
 }
+// Adding list to the local storage and in the array of todoList
 export default function addList(){
     console.log("Hello");
     const title = document.getElementById('taskName').value;
     const date = document.getElementById('taskDate').value;
     const note = document.getElementById('taskNote').value;
-    // addListToTodolist(title.value, date.value, note.value);
+    
     event.preventDefault();
     closeForm();
     resetForm();
@@ -43,13 +41,14 @@ export default function addList(){
     todoList = [...existingData, new List(title, date, note)];
     console.log(todoList);
     localStorage.setItem('todoList', JSON.stringify(todoList));
-    // listCard();
+   
     console.log("sihasj",todoList);
     const page = (document.querySelector('.inboxDiv').innerHTML = '');
     displayToPage();
+    // prevent reload when list is added
     window.location.reload();
     
 }
-// console.log(todoList);
+
 
 
